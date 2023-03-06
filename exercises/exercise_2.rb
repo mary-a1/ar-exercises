a31aa3ae09315e51ec79b8631998e51f63ce2dfb
Maryan <maryanay2000@gmail.com>
2023-03-06 18:25:03 -0500

    Update the first store

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,15 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+def update
+
+  @store1 = Store.find_by(id: 1)
+  @store1.update(name:"Adidas")
+  
+
+  @store2 = Store.find_by(id: 2)
+
+
+end
+
+update()
